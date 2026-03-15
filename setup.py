0d3070e899d004369bcdb2121936b29e358a0b42
Alice Cryer <alicecryer94@gmail.com>
2026-03-15 09:22:51 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setuptools.setup(
     version="0.1.0",
     packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
-    test_suite="tests",
+    py_modules=["destinationAPI", "main"],
+    test_suite="test",
     install_requires=[],
     extras_require={
         "dev": ["pytest", "pytest-cov"]
